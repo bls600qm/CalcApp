0a44a34523e289a7532dffa97c56811afbf77b61
rikasumitomo <lmcmacf@gmail.com>
2017-08-19 20:24:17 +0900

    例外処理

--- app/src/main/java/jp/techachademy/rika/sumitomo/calcapp/MainActivity.java
+++ app/src/main/java/jp/techachademy/rika/sumitomo/calcapp/MainActivity.java
@@ -1,6 +1,7 @@
 package jp.techachademy.rika.sumitomo.calcapp;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -32,40 +33,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button4 = (Button)findViewById(R.id.waru);
         button4.setOnClickListener(this);
 
+
         mEditText = (EditText)findViewById(R.id.editText);
         mEditText2 = (EditText)findViewById(R.id.editText2);
     }
 
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
 
-        String text = mEditText.getText().toString();
-        String text2 = mEditText2.getText().toString();
+        try {
+            String text = mEditText.getText().toString();
+            String text2 = mEditText2.getText().toString();
 
-        float value = Float.parseFloat(text);
-        float value2 = Float.parseFloat(text2);
+            float value = Float.parseFloat(text);
+            float value2 = Float.parseFloat(text2);
 
-        if(v.getId() == R.id.tasu){
-            answer = value + value2;
-        }else if(v.getId() == R.id.hiku){
-            answer = value - value2;
-        }else if(v.getId() == R.id.kakeru){
-            answer = value * value2;
-        }else{
-            answer = value / value2;
+            if (v.getId() == R.id.tasu) {
+                answer = value + value2;
+            } else if (v.getId() == R.id.hiku) {
+                answer = value - value2;
+            } else if (v.getId() == R.id.kakeru) {
+                answer = value * value2;
+            } else {
+                answer = value / value2;
+            }
+            Log.d("UI_PARTS", String.valueOf(answer));
+
+
+            Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra("watasu", answer);
+            startActivity(intent);
+        } catch (NullPointerException ex) {
+            String text = "0";
+            String text2 = "0";
+            showAlertDialog();
         }
-        Log.d("UI_PARTS",String.valueOf(answer));
 
-        if(mEditText==null ||mEditText2==null){
-            answer=0;
+}
+    private void showAlertDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("エラー");
+        alertDialogBuilder.setMessage("数値を入力してください");
 
-        }
-
-        Intent intent = new Intent(this,SecondActivity.class);
-        intent.putExtra("watasu",answer);
-        startActivity(intent);
-
-
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
-
 }
